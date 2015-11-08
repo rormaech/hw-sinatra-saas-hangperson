c830b3d2fbbcf73fc1c27a518aff3b8abb8031d3
rormaech <rormaech@gmail.com>
2015-11-08 19:26:25 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    flash[:message]="You have already used that letter!"
     erb :show # You may change/remove this line
   end
   
